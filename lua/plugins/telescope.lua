a22aa7eaffb1eff8ce01a6e479c4197ad7af4a3c
TARS-90 <grzywiastyyy@gmail.com>
2026-05-04 23:00:57 +0200

    Trying to change Telescope theme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,21 @@ return {
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		version = "0.1.8", -- last version that works
 		config = function()
+			local theme = {
+				TelescopeNormal = { bg = "none" },
+				TelescopeBorder = { bg = "none", fg = "#ffffff" },
+				TelescopePromptNormal = { bg = "none" },
+				TelescopePromptBorder = { bg = "none", fg = "#ff007c" },
+				TelescopePreviewNormal = { bg = "none" },
+				TelescopePreviewBorder = { bg = "none" },
+				TelescopeSelection = { link = "Visual" },
+				TelescopeMatching = { fg = "#ff007c", bold = true },
+			}
+
+			for group, settings in pairs(theme) do
+				vim.api.nvim_set_hl(0, group, settings)
+			end
+
 			require("telescope").setup({
 				defaults = {
 					preview = { treesitter = false, },
